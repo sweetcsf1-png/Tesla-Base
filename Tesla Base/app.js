3d66d11119b5f1db94ecdfb7f2567da472e35868
sweetcsf1-png <sweetcsf1@gmail.com>
2026-02-27 06:48:55 +0700

    Update app.js

--- Tesla Base/app.js
+++ Tesla Base/app.js
@@ -1,6 +1,6 @@
 // Replace with YOUR keys from Supabase Dashboard
 const SUPABASE_URL = 'https://your-project.supabase.co';
-const SUPABASE_KEY = 'your-anon-key'ggrvkcxnizchfbzfwvjv
+const SUPABASE_KEY = 'ggrvkcxnizchfbzfwvjv'
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // LOGIN LOGIC
@@ -34,3 +34,4 @@ async function invest(planName) {
     }
 
 }
+
